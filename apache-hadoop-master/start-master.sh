686c4b1ce68e1be98aa46385de0f8a8c41e31705
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2015-12-07 15:27:09 -0800

    Consolidate down resource manager and namenode images.

--- apache-hadoop-master/start-master.sh
+++ apache-hadoop-master/start-master.sh
@@ -15,5 +15,14 @@ $HADOOP_PREFIX/bin/hdfs namenode -format $1
 # start the namenode daemon
 $HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode
 
+# start the resource manager
+$HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager
+
+# start the proxy server
+$HADOOP_YARN_HOME/sbin/yarn-daemon.sh start proxyserver --config $HADOOP_CONF_DIR
+
+# start the job history server
+$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver --config $HADOOP_CONF_DIR
+
 # follow some logs forever
 tail -f /opt/apache-hadoop/logs/*
