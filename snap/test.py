3c71ee3e34bad386da03a74d7f930c2330c59a2f
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2017-08-30 19:50:13 -0700

    Update to latest (1.0dev100) release of snap (resolves #261)

--- snap/test.py
+++ snap/test.py
@@ -9,7 +9,7 @@ class TestSNAP(unittest.TestCase):
 
     def test_docker_call(self):
         out, err = check_docker_output('quay.io/ucsc_cgl/snap:latest')
-        self.assertTrue('Welcome to SNAP version 1.0beta.18.' in out)
+        self.assertTrue('Welcome to SNAP version 1.0dev.100.' in out)
 
 def check_docker_output(cmd):
     command = 'docker run ' + cmd
